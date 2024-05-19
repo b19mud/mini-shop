9d6ff9c79ee4cd018b24299dff82c3567e380da1
b19mud <bigmudhaijian@gmail.com>
2024-05-19 22:04:49 +0800

    同步获取标签

--- frontend/miniprogram/pages/classification/classification.ts
+++ frontend/miniprogram/pages/classification/classification.ts
@@ -17,7 +17,7 @@ Page({
    */
   onLoad() {
     // todo 获取分类信息
-    this.getAllClassification()
+    this.addAllClassification()
   },
 
   /**
@@ -32,53 +32,90 @@ Page({
    */
   onShow() {
     this.getTabBar().setData({active:"classification"})
+    console.log(this.data.rightdata)
   },
 
   getAllCategorysWithClassification(id) {
-    wx.request({
-      url: 'http://127.0.0.1:8080/category',
-      method: "POST",
-      data: {
-        "ClassificationId": id
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: (res) => {
-        console.log(res.data)
-        let info_data = this.data.infos
-        let categorys = []
-        for (let item of res.data.CategoryList) {
-          categorys.push({
-            name: item.GoodsCategoryName
-          })
+    console.log(5)
+    return new Promise((resolve, rejact)=>{
+      wx.request({
+        url: 'http://127.0.0.1:8080/category',
+        method: "POST",
+        data: {
+          "ClassificationId": id
+        },
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success (res) {
+          resolve(res)
+          // console.log(res.data)
+          // let info_data = this.data.infos
+          // let categorys = []
+          // for (let item of res.data.CategoryList) {
+          //   categorys.push({
+          //     name: item.GoodsCategoryName
+          //   })
+          // }
+          // info_data[id] = categorys
+          // this.setData({infos:info_data})
+        },
+        fail (err) {
+          rejact(err)
         }
-        info_data[id] = categorys
-        this.setData({infos:info_data})
-      }
+      })
     })
   },
 
+  async addAllClassification() {
+    console.log(1)
+    const res = await this.getAllClassification()
+    console.log(3)
+    let classificatioins = res.data.ClassificationList
+    let itemDatas = []
+    for (let item of classificatioins) {
+      itemDatas.push({
+        id: item.Id,
+        text: item.GoodsClassificationName
+      })
+    }
+    this.setData({items: itemDatas})
+
+    for (let item of this.data.items) {
+      console.log(4)
+      const categoryRes = await this.getAllCategorysWithClassification(item.id)
+      console.log(6)
+      let info_data = this.data.infos
+      let categorys = []
+      for (let item of categoryRes.data.CategoryList) {
+        categorys.push({
+          name: item.GoodsCategoryName
+        })
+      }
+      info_data[item.id] = categorys
+      this.setData({infos:info_data})
+    }
+    let mainActiveIndex = this.data.mainActiveIndex;
+    this.setData({rightdata : this.data.infos[mainActiveIndex+1]})
+  },
+
   getAllClassification() {
-    wx.request({
+    console.log(2)
+    return new Promise((resolve, rejact)=>{
+      wx.request({
         url: 'http://127.0.0.1:8080/classification',
         method: "POST",
         data: {},
         header: {
           'content-type': 'application/json' // 默认值
         },
-        success: (response) => {
-          let classificatioins = response.data.ClassificationList
-          let itemDatas = []
-          for (let item of classificatioins) {
-            itemDatas.push({
-              id: item.Id,
-              text: item.GoodsClassificationName
-            })
-            this.getAllCategorysWithClassification(item.Id)
-          }
-          this.setData({items: itemDatas})
+        success (response) {
+          resolve(response)
+        },
+        fail (err) {
+          rejact(err)
         }
+      })
     })
   },
 
