dcd46c3952e1930cf3730972f62c3a2911ee1479
b19mud <bigmudhaijian@gmail.com>
2024-05-20 22:18:19 +0800

    自定义购物车卡片组件

---
+++ frontend/miniprogram/component/cart-card/index.ts
@@ -0,0 +1,58 @@
+// component/cart-card.ts
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    num: {
+      type: String,
+      value: '1'
+    },
+    price: {
+      type: String,
+      value: '0'
+    },
+    title: {
+      type: String,
+      value: "测试"
+    },
+    desc: {
+      type: String,
+      value: '0'
+    },
+    currency: {
+      type: String,
+      value: '0'
+    },
+    tag: {
+      type: String,
+      value: '0'
+    },
+    tags: {
+      type: Array<String>,
+      value: []
+    },
+    thumb_link: {
+      type: String,
+      value: ''
+    },
+    thumb: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
